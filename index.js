548dab96fa1094020001e25e75d052b4a8a1c6d8
Joe Rehfuss <jrehfuss@c02g81atq05q.domain>
2023-01-27 13:54:19 -0800

    setup for heroku

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -9,6 +11,6 @@ app.use(express.static("public"));
 const allRoutes = require("./controllers");
 app.use(allRoutes)
 
-app.listen(3000, function () {
-  console.log("listenin on port 3000!");
+app.listen(PORT, function () {
+  console.log("listenin on port " + PORT);
 });
