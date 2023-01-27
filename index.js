890f283ea3e4a7268109c9ca04633513aee98265
Joe Rehfuss <jrehfuss@c02g81atq05q.domain>
2023-01-27 11:27:19 -0800

    updates

--- index.js
+++ index.js
@@ -1,66 +1,86 @@
 const express = require("express");
-const app  = express();
-const path = require("path")
+const app = express();
+const path = require("path");
+const fs = require("fs");
 
-let petsData=require("./pets.json")
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.static("public"));
 
-app.use(express.static("public"))
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/index.html"));
+});
 
-app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname,"./views/index.html"))
-})
-
-app.get("/pets",(req,res)=>{
-    res.json(petsData)
-})
-
-app.post("/pets",(req,res)=>{
-    console.log(req.body);
-    petsData.push(req.body)
-    res.send("post request recieved")
-})
-
-app.get("/pets/:id",(req,res)=>{
-    for (let i = 0; i < petsData.length; i++) {
-        const pet = petsData[i];
-        if(pet.id==req.params.id){
-            return res.json(pet)
-        }
+app.get("/pets", (req, res) => {
+  fs.readFile("./pets.json", "utf-8", (err, data) => {
+    if (err) {
+      res.status(500).send("oh no!");
+      throw err;
+    } else {
+      const petsData = JSON.parse(data);
+      res.json(petsData);
     }
-    return res.send("no such pet")
-})
+  });
+});
 
-app.put("/pets/:id",(req,res)=>{
-    petsData = petsData.map(pet=>{
-        if(pet.id==req.params.id){
-            return {
-                id:req.body.id,
-                name:req.body.name,
-                species:req.body.species,
-                owner:req.body.owner
-            }
+app.post("/pets", (req, res) => {
+  fs.readFile("./pets.json", "utf-8", (err, data) => {
+    if (err) {
+      res.status(500).send("oh no!");
+      throw err;
+    } else {
+      const petsData = JSON.parse(data);
+      petsData.push(req.body);
+      fs.writeFile("./pets.json", JSON.stringify(petsData, null, 4), (err) => {
+        if (err) {
+          res.status(500).send("oh no!");
+          throw err;
         } else {
-            return pet
+          res.send("data added!");
         }
-    })
-    res.send("updated")
-})
+      });
+    }
+  });
+});
 
-app.delete("/pets/:id",(req,res)=>{
-    petsData = petsData.filter(pet=>{
-        if(pet.id==req.params.id){
-            return false
-        } else {
-            return true
-        }
-    })
-    res.send("pet deleted!")
-})
+app.get("/pets/:id", (req, res) => {
+  for (let i = 0; i < petsData.length; i++) {
+    const pet = petsData[i];
+    if (pet.id == req.params.id) {
+      return res.json(pet);
+    }
+  }
+  return res.send("no such pet");
+});
 
-app.listen(3000,function(){
-    console.log("listenin on port 3000!")
-})
+app.put("/pets/:id", (req, res) => {
+  petsData = petsData.map((pet) => {
+    if (pet.id == req.params.id) {
+      return {
+        id: req.body.id,
+        name: req.body.name,
+        species: req.body.species,
+        owner: req.body.owner,
+      };
+    } else {
+      return pet;
+    }
+  });
+  res.send("updated");
+});
 
+app.delete("/pets/:id", (req, res) => {
+  petsData = petsData.filter((pet) => {
+    if (pet.id == req.params.id) {
+      return false;
+    } else {
+      return true;
+    }
+  });
+  res.send("pet deleted!");
+});
+
+app.listen(3000, function () {
+  console.log("listenin on port 3000!");
+});
--- public/js/fetchPets.js
+++ public/js/fetchPets.js
@@ -6,3 +6,28 @@ fetch("/pets").then(res=>res.json()).then(data=>{
         document.querySelector("#petList").append(myLi)
     })
 })
+
+document.querySelector("#addPetForm").addEventListener("submit",e=>{
+    e.preventDefault();
+    const newPetObj = {
+        id:parseInt(document.querySelector("#petId").value),
+        name:document.querySelector("#petName").value,
+        species:document.querySelector("#petSpecies").value,
+        owner:document.querySelector("#petOwner").value,
+    }
+    console.log(newPetObj)
+    fetch("/pets",{
+        method:"POST",
+        body:JSON.stringify(newPetObj),
+        headers:{
+            "Content-Type":"application/json"
+        }
+    }).then(res=>{
+        console.log(res)
+        if(res.ok){
+           location.reload()
+        } else {
+            alert("trumpet sound")
+        }
+    })
+})
