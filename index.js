6425d479d7a599592adcae4706d5aff1df5a952b
Joe Rehfuss <jrehfuss@c02g81atq05q.domain>
2023-01-27 11:58:07 -0800

    updates

--- index.js
+++ index.js
@@ -45,40 +45,80 @@ app.post("/pets", (req, res) => {
 });
 
 app.get("/pets/:id", (req, res) => {
-  for (let i = 0; i < petsData.length; i++) {
-    const pet = petsData[i];
-    if (pet.id == req.params.id) {
-      return res.json(pet);
-    }
-  }
-  return res.send("no such pet");
+    fs.readFile("./pets.json", "utf-8", (err, data) => {
+        if (err) {
+          res.status(500).send("oh no!");
+          throw err;
+        } else {
+          const petsData = JSON.parse(data);
+          for (let i = 0; i < petsData.length; i++) {
+            const pet = petsData[i];
+            if (pet.id == req.params.id) {
+              return res.json(pet);
+            }
+          }
+          return res.send("no such pet");
+        }
+      });
 });
 
 app.put("/pets/:id", (req, res) => {
-  petsData = petsData.map((pet) => {
-    if (pet.id == req.params.id) {
-      return {
-        id: req.body.id,
-        name: req.body.name,
-        species: req.body.species,
-        owner: req.body.owner,
-      };
-    } else {
-      return pet;
-    }
-  });
-  res.send("updated");
+    fs.readFile("./pets.json", "utf-8", (err, data) => {
+        if (err) {
+          res.status(500).send("oh no!");
+          throw err;
+        } else {
+          let petsData = JSON.parse(data);
+          petsData = petsData.map((pet) => {
+            if (pet.id == req.params.id) {
+              return {
+                id: req.body.id,
+                name: req.body.name,
+                species: req.body.species,
+                owner: req.body.owner,
+              };
+            } else {
+              return pet;
+            }
+          });
+          fs.writeFile("./pets.json", JSON.stringify(petsData, null, 4), (err) => {
+            if (err) {
+              res.status(500).send("oh no!");
+              throw err;
+            } else {
+              res.send("data updated!");
+            }
+          });
+        }
+      });
 });
 
 app.delete("/pets/:id", (req, res) => {
-  petsData = petsData.filter((pet) => {
-    if (pet.id == req.params.id) {
-      return false;
-    } else {
-      return true;
-    }
-  });
-  res.send("pet deleted!");
+    fs.readFile("./pets.json", "utf-8", (err, data) => {
+        if (err) {
+          res.status(500).send("oh no!");
+          throw err;
+        } else {
+          let petsData = JSON.parse(data);
+          petsData = petsData.filter((pet) => {
+            if (pet.id == req.params.id) {
+              return false;
+            } else {
+              return true;
+            }
+          });
+          fs.writeFile("./pets.json", JSON.stringify(petsData, null, 4), (err) => {
+            if (err) {
+              res.status(500).send("oh no!");
+              throw err;
+            } else {
+              res.send("data deleted!");
+            }
+          });
+        }
+      });
+ 
+
 });
 
 app.listen(3000, function () {
